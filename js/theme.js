819f15acc28e5964395c55b04e8c2e9d3f82477a
Maikel Koopman <maikel.koopman@goalgorilla.com>
2015-12-24 11:40:53 +0100

    added form to mycpnb, theme selects, theme textarea theme form submit

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,30 @@
 (function ($) {
 
   /*
+   * Wrap selects into a themable div
+   */
+
+  Drupal.behaviors.enhanceForms = {
+    attach: function (context, settings) {
+
+      // Pass focused state to visible parent element.
+      $('.form-item .form-select')
+        .wrap('<div class="selector"></div>')
+        .focus(function(){
+          $(this).parent('.selector').addClass('focused');
+        })
+        .blur(function(){
+          $(this).parent('.selector').removeClass('focused');
+        });
+
+    }
+
+  };
+
+})(jQuery);
+(function ($) {
+
+  /*
    * Mobile menu behavior
    */
   Drupal.behaviors.mobileNav = {
