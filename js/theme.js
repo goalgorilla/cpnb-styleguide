f84ae222779cd9e7e85ef53eff2eabce90678b3d
Maikel Koopman <maikel.koopman@goalgorilla.com>
2016-02-23 15:14:25 +0100

    Update 2016-02-23T14:14:23.450Z

--- js/theme.js
+++ js/theme.js
@@ -56,6 +56,24 @@
 (function ($) {
 
   /*
+   * Emphasis on external links
+   */
+  Drupal.behaviors.externalLink = {
+    attach: function (context, settings) {
+
+      // add class to external links so they can be styled
+      $('a.teaser--event').filter(function() {
+        return this.hostname && this.hostname !== location.hostname;
+      }).attr('target', '_blank').addClass('external-link');
+
+    }
+  };
+
+})(jQuery);
+
+(function ($) {
+
+  /*
    * Mobile menu behavior
    */
   Drupal.behaviors.mobileNav = {
